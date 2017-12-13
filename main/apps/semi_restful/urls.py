e0723c85e147a133175857d0d38af4d543755e58
Paul <paulwinegard@gmail.com>
2017-12-13 10:54:41 -0800

    Initial routes established.  Add user process and html routes added.

--- main/apps/semi_restful/urls.py
+++ main/apps/semi_restful/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^add$', views.add),
+    url(r'^add_user$', views.add_user)
+
 ]
--- main/apps/semi_restful/views.py
+++ main/apps/semi_restful/views.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import *
 
 def index(request):
-    return render(request, 'index.html')
+    all_users = User.objects.all()
+    context = {
+        "users" : all_users
+    }
+    return render(request, 'index.html', context)
+
+def add(request):
+    return render(request, "add.html")
+def add_user(request):
+    return redirect("/")
 # Create your views here.
