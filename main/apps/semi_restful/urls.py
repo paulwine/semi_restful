9e58d595453e3b92a57f0330fbd5e01638e2bea9
Paul <paulwinegard@gmail.com>
2017-12-13 12:47:04 -0800

    User input, edit and delete all routed and functional.  Minor changes needed if any

--- main/apps/semi_restful/urls.py
+++ main/apps/semi_restful/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^add$', views.add),
-    url(r'^add_user$', views.add_user)
+    url(r'^add_user$', views.add_user),
+    url(r'^edit/(?P<user_id>\d+)$', views.edit),
+    url(r'^edit_user$', views.edit_user),
+    url(r'^delete/(?P<user_id>\d+)$', views.delete)
 
 ]
--- main/apps/semi_restful/views.py
+++ main/apps/semi_restful/views.py
@@ -14,5 +14,36 @@ def index(request):
 def add(request):
     return render(request, "add.html")
 def add_user(request):
+    if request.method == "POST":
+        first = request.POST["first_name"]
+        last = request.POST["last_name"]
+        email = request.POST["email"]
+        User.objects.create(first_name = first, last_name=last, email= email)
+        return redirect("/")
+    else:
+        return redirect('/add')
     return redirect("/")
+
+def edit(request, user_id):
+    request.session["user_id"] = user_id
+    
+    
+    return render(request, "edit.html")
+def edit_user(request):
+    
+    if request.method == "POST":
+        first = request.POST["first_name"]
+        last = request.POST["last_name"]
+        email = request.POST["email"]
+        User.objects.filter(id = request.session["user_id"]).update(first_name = first, last_name=last, email= email)
+        return redirect("/")
+    else:
+        return redirect('/add')
+    return redirect("/")
+
+def delete(request, user_id):
+    print user_id
+    User.objects.filter(id = user_id).delete()
+    return redirect("/")
+
 # Create your views here.
